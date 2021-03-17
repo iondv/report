42c65b227c8cb722116e56b8d959aa461209ee49
Dan Krasilnikov <krasilneg@yandex.ru>
2021-03-17 20:31:37 +1000

    fix

--- config/index.js
+++ config/index.js
@@ -3,8 +3,17 @@
  */
 'use strict';
 
-var read = require('@iondv/commons/lib/config-reader');
-var config = require('./config.json');
+const fs = require('fs');
+const path = require('path');
+const read = require('@iondv/commons/lib/config-reader');
+const { readConfig } = require('@iondv/core/utils/read');
+const config = readConfig(path.join(__dirname, './config.json'));
 
-module.exports = read(config, __dirname);
+let ini_dir = process.env.ION_CONFIG_PATH || 'config';
+
+ini_dir = path.isAbsolute(ini_dir)
+  ? ini_dir
+  : path.normalize(path.join(process.cwd(), ini_dir));
+
+module.exports = read(config, fs.existsSync(ini_dir) ? ini_dir : null);
 
