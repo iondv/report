2a8ca5116bbd15232a23778044751d94cfaf57d1
Dan Krasilnikov <krasilneg@yandex.ru>
2021-03-17 18:09:08 +1000

    fix

---
+++ bin/import.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const importer = require('../lib/import');
+
+const params = {
+  src: process.cwd(),
+  ns: null
+};
+
+let setParam = null;
+
+process.argv.forEach(function (val) {
+  if (val.substr(0, 2) === '--') {
+    setParam = val.substr(2);
+  } else if (setParam) {
+      params[setParam] = val;
+  }
+});
+
+importer(params.src, params.ns)
+  .then(() => console.log('Import done'))
+  .catch((err) => console.error(err))
